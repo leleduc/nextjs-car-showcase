020631b03e9ebb0d2627b0f7d006776c7089dfe0
Duc Anh <leleduc@gmail.com>
2023-07-24 15:46:15 +0700

    Search Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Hero } from '@/components';
-import Image from 'next/image';
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+      </div>
+      <div className="home__filters">
+        <SearchBar />
+        <div>
+          <CustomFilter />
+        </div>
+      </div>
     </main>
   );
 }
